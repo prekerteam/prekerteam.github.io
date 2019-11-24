2c89674ef4ce8dd076801e30e4df4d4f385aabda
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-11-25 01:29:27 +0700

    Create controler.js

---
+++ js/controler.js
@@ -0,0 +1,14 @@
+
+function cmx(txt) {
+    var res = atob(txt);
+    return (res);
+}
+function mcx(txt) {
+    var res = btoa(txt);
+    return (res);
+}
+function cid(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1] || ''
+    );
+}
