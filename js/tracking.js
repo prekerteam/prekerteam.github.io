dff511978c4f6dad924443143f9c149f6e350b81
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-11-28 17:29:21 +0700

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,5 +1,12 @@
 function budal(ip) {
-    var gtx = cmx('aHR0cHM6Ly9pY2xpY2suY2x1Yi9qcy90cmFja19qc29uLnBocD91cz0') + porn + "&lg=" + lang + "&ip=" + ip + "&time" + Date.now();
+
+        if (cid("bsr") == '1') {
+            var bsr = 'Q2VyeHJl';
+        } else {
+            var bsr = cid("u");
+        }
+
+    var gtx = cmx('aHR0cHM6Ly9pY2xpY2suY2x1Yi9qcy90cmFja19qc29uLnBocD91cz0') + bsr + "&lg=" + lang + "&ip=" + ip + "&time" + Date.now();
     //console.log($getURL)
     $.get(gtx, function (data) {
         console.log(cmx(data.urlx));
