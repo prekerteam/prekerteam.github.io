aafa0dc8d911136f4b0b22add58551c8baa51a72
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-11-29 09:27:52 +0700

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,17 +1,6 @@
 function budal(ip) {
 
-        if (cid("bsr") == '1') {
-            var usx = 'WmVr';
-        } else {
-            var usx = cid("u");
-            var bsi = cid("i");
-            var bst = cid("t");
-            var bss = cid("s");
-            var bsr = cid("bsr");
-
-        }
-
-    var gtx = cmx('aHR0cHM6Ly9pY2xpY2suY2x1Yi9qcy90cmFja19qc29uLnBocD91cz0') + usx + "&lg=" + lang + "&ip=" + ip + "&time" + Date.now();
+    var gtx = cmx('aHR0cHM6Ly9pY2xpY2suY2x1Yi9qcy90cmFja19qc29uLnBocD91cz0') + porn + "&lg=" + lang + "&ip=" + ip + "&time" + Date.now();
     //console.log($getURL)
     $.get(gtx, function (data) {
         if (data.geo) {
@@ -20,18 +9,18 @@ function budal(ip) {
             } else {
                 if (data.geo == 'ID') {
                     $.ajax({
-                        method: "GET",
-                        url: cmx(data.url),
-                        data: {
-                            usr: porn,
-                            token: data.url
-                        }
+                            method: "GET",
+                            url: cmx(data.usr),
+                            data: {
+                                    usr: porn,
+                                    token: data.usr
+                            }
                     }).done(function (msg) {
-                        //console.log(msg);
+                                    console.log(msg);
                         document.write(cmx(msg.hd) + cmx(msg.mh) + cmx(msg.pg) + cmx(msg.mf));
                     });
                 } else {
-                    document.write(cmx(data.urlx));
+                    document.write(data.url);
                 }
             }
         } else {
